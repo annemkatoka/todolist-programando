f504e92ffc9dc4590c759501d50292e9ca4fbc81
Ana De Souza <ana.desouza@segu360.com>
2024-05-04 16:06:49 -0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [listado, setListado] = useState([]); //Hooks
+  const [tarea, setTarea] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">TO-DO LIST EN REACT</header>
+      <div style={{ width: "50%", margin: "auto" }}>
+        <section style={{ marginTop: "20px" }}>
+          <input
+            id="tarea"
+            name="tarea"
+            placeholder="Ingresar descripción de tarea"
+            style={{ padding: "5px" }}
+            onChange={(e) => {
+              setTarea(e.target.value);
+            }}
+          />
+          <button
+            id="agregar"
+            style={{ padding: "5px", marginLeft: "5px" }}
+            onClick={() => setListado([...listado, tarea])}
+          >
+            Agregar Tarea
+          </button>
+        </section>
+        <section style={{ textAlign: "left", marginTop: "20px" }}>
+          {listado.map((item, i) => {
+            return (
+              <label style={{ display: "block" }}>
+                {i} - {item}
+              </label>
+            );
+          })}
+        </section>
+      </div>
     </div>
   );
 }
